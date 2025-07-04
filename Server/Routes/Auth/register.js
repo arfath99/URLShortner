6197879771dbd41fa35a9ec8eabfad1ec2465e8d
arfath99 <arfathsyed80@gmail.com>
2025-07-04 22:36:16 +0530

    Cleanup-Login-Register

--- Server/Routes/Auth/register.js
+++ Server/Routes/Auth/register.js
@@ -1,5 +1,12 @@
+import bcrypt from 'bcrypt';
+import zxcvbn from 'zxcvbn';
+
+import client from '#db/client.js';
+
 import registerSchema from '#schemas/registerUserSchema.js';
-import createUser from '#utility/Users/registerUser.js';
+
+import successResponse from '#utility/Responses/successResponse.js';
+import errorResponse from '#utility/Responses/errorResponse.js';
 
 async function register(app, options) {
   app.post('/register', { schema: registerSchema }, async (req, res) => {
@@ -9,4 +16,36 @@ async function register(app, options) {
   });
 }
 
+async function createUser({ username, email, password }) {
+  try {
+    const user = await client.query('SELECT ID FROM USERS WHERE EMAIL = $1', [
+      email,
+    ]);
+
+    if (user.rows.length) {
+      return errorResponse(409, 'User already exists');
+    }
+
+    const passwordStrength = zxcvbn(password);
+
+    if (passwordStrength.score <= 2) {
+      return errorResponse(400, 'Weak Password');
+    }
+
+    const saltedPassword = await bcrypt.hash(password, 10);
+    await client.query(
+      'INSERT INTO USERS(USERNAME, EMAIL, PASSWORD) VALUES ($1, $2, $3)',
+      [username, email, saltedPassword]
+    );
+
+    return successResponse(201, 'User Created Successfully');
+  } catch (error) {
+    return errorResponse(500, 'Failed to create user', error);
+  }
+}
+
 export default register;
+
+/**
+ * Handler Name
+ */
