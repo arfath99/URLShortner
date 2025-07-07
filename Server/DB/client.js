89a01565e9c631f51eb3335d11a1033e710b10fb
arfath99 <arfathsyed80@gmail.com>
2025-07-07 08:49:47 +0530

    Connection-Pooling

--- Server/DB/client.js
+++ Server/DB/client.js
@@ -1,13 +1,16 @@
-import { Client, Result } from 'pg';
+import Pool from 'pg-pool';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const client = new Client({
+const client = new Pool({
   user: process.env.DB_USERNAME,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  max: process.env.DB_MAX_CONNECTIONS,
+  idleTimeoutMillis: process.env.DB_IDLE_TIMEOUT,
+  connectionTimeoutMillis: process.env.DB_CONNECTION_TIMEOUT,
 });
 
 await client.connect();
